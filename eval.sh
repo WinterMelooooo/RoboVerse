febab9aa41204fba796c2c0e204dd8a67fb3b6b6
geng-haoran <ghr@berkeley.edu>
2025-05-08 04:32:27 +0000

    Second Storage before switching to main

--- eval.sh
+++ eval.sh
@@ -1,4 +1,4 @@
 export WANDB_API_KEY=263d7ff027d2057a793fb7f51783d43f5b6344cc
-export CUDA_VISIBLE_DEVICES=5
-python roboverse_learn/eval.py --task CloseBox --algo diffusion_policy --max_step 500 --num_envs 1 --task_id_range_low 0 --task_id_range_high 100 --random.level 1 --headless --checkpoint_path /home/ghr/yktang/RoboVerse/info/outputs/DP/2025.05.06/01.51.03_CloseBoxFrankaL1_obs:joint_pos_act:joint_pos/checkpoints/2000.ckpt
-#python roboverse_learn/eval.py --task StackCube --algo diffusion_policy --max_step 500 --num_envs 1 --task_id_range_low 0 --task_id_range_high 200 --random.level 1 --headless --checkpoint_path /home/ghr/yktang/RoboVerse/info/outputs/DP/2025.05.06/20.36.20_rgb_resnet18_StackCubeFrankaL1_obs:joint_pos_act:joint_pos/checkpoints/200.ckpt
+export CUDA_VISIBLE_DEVICES=6
+#python roboverse_learn/eval.py --task CloseBox --algo diffusion_policy --max_step 500 --num_envs 1 --task_id_range_low 0 --task_id_range_high 100 --random.level 1 --headless --checkpoint_path /home/ghr/yktang/RoboVerse/info/outputs/DP/2025.05.06/01.51.03_CloseBoxFrankaL1_obs:joint_pos_act:joint_pos/checkpoints/2000.ckpt
+python roboverse_learn/eval.py --task StackCube --algo diffusion_policy --max_step 500 --num_envs 50 --task_id_range_low 0 --task_id_range_high 200 --random.level 0 --headless --checkpoint_path /home/ghr/yktang/RoboVerse/info/outputs/DP/2025.05.06/22.47.59_pntcloud_dp3_StackCubeFrankaL0_obs:joint_pos_act:joint_pos/checkpoints/3000.ckpt
--- vis.py
+++ vis.py
@@ -5,5 +5,5 @@ from roboverse_learn.algorithms.utils.visualizer import visualizer
 
 idx = np.random.randint(0, 47441)
 pnt_cloud = np.load(f"/home/ghr/yktang/RoboVerse/tmp/visualize/StackCubeL2/pnt_cloud/{idx}.npy")
-print(idx)
+# print(idx)
 visualizer.visualize_pointcloud(pnt_cloud)
