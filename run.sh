95e1e3354062a7760958a231b8494467117fd106
WinterMelooooo <2538358797@qq.com>
2025-05-15 04:50:32 +0000

    switching to main

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export WANDB_API_KEY=263d7ff027d2057a793fb7f51783d43f5b6344cc
-bash roboverse_learn/algorithms/diffusion_policy/train_dp.sh roboverse_demo/demo_isaaclab/StackCube-Level0/robot-franka StackCubeFrankaL0 100 1,2,3,4,5,6,7 2000 joint_pos joint_pos 0 1 1 robot_dp_pntcloud_spUnet 1 50048 50 sqrt
+bash roboverse_learn/algorithms/diffusion_policy/train_dp.sh roboverse_demo/demo_isaaclab/StackCube-Level1/robot-franka StackCubeFrankaL1 100 1,2,3,4 2000 joint_pos joint_pos 0 1 1 robot_dp_rgbd_vit 1 50048 50 sqrt
 #bash roboverse_learn/algorithms/diffusion_policy/train_dp.sh roboverse_demo/demo_isaaclab/StackCube-Level0/robot-franka StackCubeFrankaL0 100 4,5,6,7 2000 joint_pos joint_pos 0 1 1 robot_dp_pntcloud 1 50036 100 sqrt
 #bash roboverse_learn/algorithms/diffusion_policy/train_dp.sh roboverse_demo/demo_isaaclab/StackCube-Level/robot-franka StackCubeFrankaL0 100 2,3,4,5 2000 joint_pos joint_pos 0 1 1 robot_dp_pntcloud_spUnet 1 50043 100 linear 16 2 8
