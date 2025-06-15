513681343390d6babff247d276a83f9f7f4959c0
WinterMelooooo <2538358797@qq.com>
2025-06-15 23:27:26 +0000

    implemented multimodal_enc pro

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 export WANDB_API_KEY=263d7ff027d2057a793fb7f51783d43f5b6344cc
-task_name=CloseBox
+task_name=StackBox
 level=0
-config_name=robot_dp_tactile_resnet_pointnet_token_attention_pool
+config_name=robot_dp_tactile_resnet_pointnet_token_attention_cls
 num_epochs=2000
-port=50063
+port=50065
 norm_pnt_cloud=0
 seed=42
-gpus=4,5,6,7
+gpus=0,1,2,3,4,5,6,7
 strategy=sqrt
 train_ratio=100
 logger=wandb
