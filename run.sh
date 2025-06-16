4bd8063be051711bb53b51ac990b6c20034615b3
WinterMelooooo <2538358797@qq.com>
2025-06-16 19:26:21 +0000

    sync with slurm

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 export WANDB_API_KEY=263d7ff027d2057a793fb7f51783d43f5b6344cc
-task_name=StackCube
+task_name=CloseBox
 level=0
-config_name=robot_dp_resnet_pointnet_mutual_attention_pro_modal_encoding
+config_name=robot_dp_tactile_resnet_pointnet_token_attention_cls
 num_epochs=2000
-port=50042
+port=50044
 norm_pnt_cloud=0
 seed=42
 gpus=0,1,2,3,4,5,6,7
